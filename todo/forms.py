208b1c062c47f934bae50134ef9ee39fc2562590
Adrian Drinceanu <drinceanuadrian@gmail.com>
2024-01-23 12:27:13 +0200

    added checkbox items in forms

--- todo/forms.py
+++ todo/forms.py
@@ -1,8 +1,13 @@
 
 from django import forms
-from .models import Todo
+from .models import Todo, CheckboxItem
 
 class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = "__all__"
+        
+class CheckboxItemForm(forms.ModelForm):
+    class Meta:
+        model = CheckboxItem
+        fields = ['title', 'completed']
