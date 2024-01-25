a297004b1c5ccca4bafd1d90ff1c3e687b74fed8
Adrian Drinceanu <drinceanuadrian@gmail.com>
2024-01-25 10:19:44 +0200

    changed the urls config to /todo

--- ToDoWebApp/urls.py
+++ ToDoWebApp/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from todo import views
 
 urlpatterns = [
     #####################home_page###########################################
-    path('', views.index, name="todo"),
+    path('todo/', include('todo.urls')),
     ####################give id no. item_id name or item_id=i.id ############
     # pass item_id as primary key to remove that the todo with given id
     path('del/<str:item_id>', views.remove, name="del"),
